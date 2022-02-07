4711894bb4b1b825543971b8d61b664997353e88
AlekseyJS <alekseygnatenko@gmail.com>
2022-02-07 21:18:00 +0300

    HW1 ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,71 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int n = 0; n < storage.length; n++){
+            if (storage[n] == null){
+                storage[n] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (Resume r : storage) {
+            if (r == null){
+                break;
+            }
+            if (uuid.equals(r.uuid)) {
+                return r;
+            }
+
+        } return null;
     }
 
     void delete(String uuid) {
+        for (int n = 0; n < storage.length; n++){
+            if (storage[n] == null){
+                break;
+            }else if (uuid.equals(storage[n].uuid)) {
+                if (storage[n + 1] == null){
+                    storage[n] = null;
+                }else{
+                    if (storage.length - 1 - n >= 0)
+                        System.arraycopy(storage, n + 1, storage, n, storage.length - 1 - n);
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int i = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        return Arrays.copyOfRange(storage, 0, i);
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        return i;
     }
 }
