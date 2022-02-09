94547a735b3afeea7e6079482fe77a8606f8a1a6
AlekseyJS <alekseygnatenko@gmail.com>
2022-02-09 16:21:41 +0300

    HW1 ArrayStorage
    - Из всех методов исключена проверка элементов массива на null значение.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,39 +13,26 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume r : storage) {
-            if (r == null) {
-                break;
-            } else if (uuid.equals(r.uuid)) {
-                return r;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                break;
-            } else if (uuid.equals(storage[i].uuid)) {
-                if (storage[i + 1] == null) {
-                    storage[i] = null;
-                } else {
-                    for (int j = i; storage[j] != null; j++) {
-                        storage[j] = storage[j + 1];
-                    }
-                }
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = null;
+                System.arraycopy(storage, i + 1, storage, i, size - i);
                 size--;
+                break;
             }
         }
     }
@@ -54,7 +41,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
+        return Arrays.copyOfRange(storage, 0, size + 5);
     }
 
     int size() {
