5f37c1ddde519e783d3071d31cea1653027aa1c2
ghJS90 <alekseygnatenko@gmail.com>
2022-09-07 14:23:18 +0300

    HW11
    добавлен класс TestDeadlock
    исправлено дублирование кода

--- src/com/urise/webapp/TestDeadlock.java
+++ src/com/urise/webapp/TestDeadlock.java
@@ -4,40 +4,24 @@ public class TestDeadlock {
     public static final Object o1 = new Object();
     public static final Object o2 = new Object();
 
+    public static void testMethod(Object obj1, Object obj2){
+        synchronized (obj1) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (obj2) {
+                System.out.println("Не увидим это сообщение");
+            }
+        }    }
+
     public static void main(String[] a) {
-        Thread t1 = new Thread1();
-        Thread t2 = new Thread2();
+
+        Thread t1 = new Thread(() -> testMethod(o1, o2));
+        Thread t2 = new Thread(() -> testMethod(o2, o1));
+
         t1.start();
         t2.start();
-    }
-
-    private static class Thread1 extends Thread {
-        public void run() {
-            synchronized (o1) {
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (o2) {
-                    System.out.println("Не увидим это сообщение");
-                }
-            }
-        }
-    }
-
-    private static class Thread2 extends Thread {
-        public void run() {
-            synchronized (o2) {
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (o1) {
-                    System.out.println("Не увидим это сообщение");
-                }
-            }
-        }
     }
 }
