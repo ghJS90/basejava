4737bfe5ad36fb3fd2503ba906065308f4aef5fe
ghJS90 <alekseygnatenko@gmail.com>
2022-09-10 17:53:38 +0300

    HW12
    1я часть ДЗ

---
+++ src/com/urise/webapp/HW12.java
@@ -0,0 +1,18 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class HW12 {
+    public static void main(String[] args) {
+        int[] array = new int[]{9, 1, 3, 5};
+        System.out.println(minValue(array));
+    }
+
+    public static int minValue(int[] arr) {
+        int[] resultArray = Arrays.stream(arr).sorted().distinct().toArray();
+        return IntStream.range(0, arr.length)
+                .map(i -> (int) (Math.pow(10, resultArray.length - 1 - i) * resultArray[i]))
+                .sum();
+    }
+}
